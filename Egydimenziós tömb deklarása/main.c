c87515c5e89b40ea2ec2c492c756f352e8caded1
Pliexe <42877326+Pliexe@users.noreply.github.com>
2019-11-12 19:55:18 +0100

    Egydimenzios tomb deklarasa

---
+++ Egydimenziós tömb deklarása/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXELLEM 30
+
+int main(void)
+{
+    int A[MAXELLEM], n, i;
+
+    // ----------------
+    // Тömb deklarálása
+    // ----------------
+
+    // Lenullázás
+    for (i = 0; i < MAXELLEM; i++) // Át megy mindegyik tömb elemén
+        A[i] = 0;                  // Lenulláza a tömb elemeit
+
+    // n - vagy is tömb elemeinek száma meghatározása
+    do
+    {
+        printf("Kérem a tömb elemeinek számát: ");
+        scanf("%d", &n);
+    } while (n <= 0);
+
+    // Tömb elemeinek megadása
+    for (i = 0; i < n; i++)
+    {
+        printf("Kerem a tömb %d elemét: ", i + 1);
+        scanf("%d", &A[i]);
+    }
+
+    // Tömb elemeinek ki írása
+    for (i = 0; i < n; i++)
+        printf("A tömb %d eleme %d.", i + 1, A[i]);
+}
